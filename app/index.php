aba16a9602a80d0719d7007339b136165eb62b73
Hector <hblanco@iesebre.com>
2022-11-07 16:49:51 +0100

    Separem la configuració {config.php} del codi -> Implementant la O de SOLID -> Open To Extension

--- app/index.php
+++ app/index.php
@@ -6,16 +6,12 @@ require 'app/Task.php';
 
 require 'config.php';
 
-$user = $config['database']['user'];
-$pass = $config['database']['password'];
-$type = $config['database']['databasetype'];
-$host = $config['database']['host'];
-$name = $config['database']['name'];
-$dsn  = "$type:host=$host;dbname=$name";
-
-dd($dsn);
+//Connect -> API nom connect, sense parametres entrada i tornara objecte $dbh
 try {
-    $dbh = new PDO($dsn, $user, $pass);
+    $dbh = new PDO(
+        $config['database']['databasetype'] . ':host=' . $config['database']['host'] . ';dbname=' . $config['database']['name'],
+        $config['database']['user'],
+        $config['database']['password']);
 } catch (\Exception $e) {
    echo 'Error de connexió a la base de dades';
 }
