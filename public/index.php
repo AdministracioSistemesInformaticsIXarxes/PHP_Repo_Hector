edda3acf1874e2de33ea8e111c00e9034581fceb
Hector <hblanco@iesebre.com>
2023-03-07 17:52:46 +0100

    Debugging final dels videos

--- public/index.php
+++ public/index.php
@@ -1,10 +1,18 @@
 <?php
 
+use Framework\App;
+
 require '../vendor/autoload.php';
 
 require '../framework/bootstrap.php';
 
-require 'app/index.php';
-require 'resources/views/index.blade.php';
+//URL
+//$routes = new Route();
+//$routes->define($routes);
+//$routes->redirect($_SERVER['REQUEST_URI']);
 
+// FACADES -> IOC CONTAINER -> App
+App::get('router')->redirect($_SERVER['REQUEST_URI']);
 
+//require 'app/index.php';
+//require 'resources/views/index.blade.php';
